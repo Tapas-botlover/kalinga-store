0ebd5b03c836a313a233a64bed6b8528ece980c7
Tapas-botlover <freefireuse932@gmail.com>
2025-06-23 21:12:25 +0530

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,22 @@
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open("kalinga-store-v1").then(cache => {
-      return cache.addAll(["/", "/index.html", "/style.css", "/manifest.json"]);
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open('kalinga-store').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/style.css',
+        '/main.js',
+        '/manifest.json'
+      ]);
     })
   );
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
     })
   );
 });
